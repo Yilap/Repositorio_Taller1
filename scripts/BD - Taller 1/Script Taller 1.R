96ec5bfb0fdd029e015063c55edbb8805bec7240
Yilmer Palacios <ya.palacios@uniandes.edu.co>
2023-02-10 17:35:41 -0500

    Update Script Taller 1.R

--- scripts/BD - Taller 1/Script Taller 1.R
+++ scripts/BD - Taller 1/Script Taller 1.R
@@ -10,8 +10,11 @@
 rm(list = ls()) 
 
 #install.packages("pacman")
+install.packages("MASA")
+
+
 require("pacman")
-p_load("tidyverse","rvest","rio","skimr","caret","ggplot2","stargazer","boot")
+p_load("tidyverse","rvest","rio","skimr","caret","ggplot2","stargazer","boot", "sandwich", "ggplo2")
 
 # Importing Dataset (Webscrapping)-------------------------------------------------------
 
@@ -96,7 +99,8 @@ GEIHf$relab <- as.numeric(GEIHf$relab)
 
 stargazer(GEIHf, summary = TRUE, type = "text")
 
-#Identificamos y eliminamos outliers, estos serán los que estén 3 sd alejados de la media, creamos nuevo df
+#Identificamos y eliminamos outliers, estos serán los que estén 3 sd alejados de la media, 
+# creamos nuevo df llamado GEIHSO
 
 GEIHSO <- GEIHf
 
@@ -112,9 +116,111 @@ GEIHSO <- GEIHSO %>%
   filter(experp < mean(experp) + 3 * sd(experp) &
            experp > mean(experp) - 3 * sd(experp))
 
+#Pensar bien para presentar estadísticas descriptivas y revisar construcciòn de variables, Y: listo
 
-#Pensar bien para presentar estadísticas descriptivas y revisar construcciòn de variables
+stargazer(GEIHSO, summary = TRUE, type = "text")
 
+
+#/////////////////////////////////////////////////////////////////////////////
+
+# Age-wage profile --------------------------------------------------------
+
+#Logaritmo del salario por hora
+GEIHSO$lningresoh <- log(GEIHSO$inghora)
+
+#Edad al cuadrado
+GEIHSO$age2 <- I(GEIHSO$age^2)
+
+reg1 <- subset (GEIHSO, select = c("lningresoh","age","age2"))
+view(reg1)
+
+#Regresión lineal 
+reg_lin <- lm(lningresoh ~ age + age2, data = GEIHSO)
+
+
+# Ver el resultado de la regresión lineal
+summary(reg_lin)
+stargazer(reg_lin,type="text")
+
+
+# Relación entre los residuos de la regresión lineal y la variable dependiente 
+ggplot(reg1) + 
+  geom_point(aes(x = reg_lin$residuals, y = lningresoh)) +
+  ggtitle("Residuos de la regresión lineal vs ln ingreso por hora") +
+  labs(x = "Residuales", y = "Log de ingreso por hora")
+
+# Esto se hace para ver qué tanto los residuales se ajustan a la recta de la regresión
+# Este gráfico se utiliza comúnmente para verificar la normalidad y homogeneidad de 
+# los residuos y para asegurarse de que los supuestos del modelo estén siendo cumplidos.
+
+#Analizar la edad pico con los resultados del modelo
+coefs <- coef(reg_lin)
+p_ingh<- coefs[1]+coefs[2]* reg1$age + coefs[3]* reg1$age2
+p_ingh <- c(p_ingh)
+p_ingh<- data.frame(p_ingh)
+
+#Gráfico del perfil edad-ingresos estimado que implica la ecuación anterior
+ggplot(p_ingh) + 
+  geom_point(aes(x = reg1$age, y = p_ingh)) +
+  ggtitle("Perfil edad-ingresos") +
+  labs(x = "Edad", y = "Pred. log. de ingreso por hora")
+
+#Construcción de intervalos de confianza usando bootstrap
+age_mean<- mean(reg1$age)
+age2_mean<- mean(reg1$age2)
+betas <- reg_lin$coefficients
+view(betas)
+b0<-betas[1]
+b1<-betas[2]
+b2<-betas[3]
+
+#Set seed para asegurar replicabilidad
+set.seed(1111)
+R<-1000
+est_reg1<- rep(0,R)
+
+#Loop que calcula la edad media para la cual el ingreso por hora promedio es máximo
+
+for(i in 1: R){ age_mean<- mean(reg1$age)
+age2_mean<- mean(reg1$age2)
+ingh_sample<- sample_frac(reg1, size = 1,replace = TRUE)
+reg1_sample<- lm(lningresoh ~ age + age2, data = ingh_sample)
+betas1<- reg1_sample$coefficients
+betas1
+b0 <- betas1[1]
+b1<- betas1[2]
+b2 <- betas1[3]
+est_reg1[i]<- b1/(-2*b2)
+}
+
+#Histograma
+hist(est_reg1, main = "Edad en la que se max. ln ingreso por hora", xlab = "Edad", col = "gray")
+abline(v = mean(est_reg1), col = "red", lwd = 2)
+
+summary(est_reg1)
+
+#Intervalos de confianza
+est_boot <- function(reg1, index, 
+                     age_mean = mean(reg1$age), 
+                     age2_mean =  mean(reg1$age2) ){
+  coef(lm(lningresoh~age+ age2, data = reg1, subset = index))}
+
+bootstatistics <- boot(reg1, statistic = est_boot, R = 1000)
+bootstatistics
+
+#Cálculo de la edad pico
+edadpico <- b1/(-2*b2)
+edadpico
+
+
+
+
+
+
+
+
+
+#{//////////////////////////////////////////////////////////////////////////////
 
 # Age-wage profile --------------------------------------------------------
 
@@ -202,4 +308,5 @@ bootstatistics
 #Cálculo de la edad pico
 edadpico <- b1/(-2*b2)
 edadpico
+
 
